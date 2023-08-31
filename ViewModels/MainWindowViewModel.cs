e298e3586cca84e8f271b80fda6e8dea73a9c9b5
Michał Szyma <devszyma@gmail.com>
2023-08-31 23:25:00 +0200

    Move Event of buttons to ICommand functions in ViewModel.

--- ViewModels/MainWindowViewModel.cs
+++ ViewModels/MainWindowViewModel.cs
@@ -1,5 +1,13 @@
 ﻿using System;
+using System.Windows.Input;
 using ReactiveUI;
+using Avalonia.Interactivity;
+using Avalonia.Platform.Storage;
+using ExifEditor.ViewModels;
+using ExifLibrary;
+using Avalonia.Controls;
+using Avalonia.Controls.ApplicationLifetimes;
+using System.IO;
 
 namespace ExifEditor.ViewModels;
 
@@ -7,20 +15,70 @@ public class MainWindowViewModel : ViewModelBase
 {
     private readonly AppSettings appSettings;
 
+    private string? _exifDescription;
+    public ICommand _selectDirectoryCommand;
+    public ICommand _showExifCommand;
+
     public MainWindowViewModel() {
         appSettings = SettingsService.LoadSettings();
     }
 
+    public ICommand SelectDirectoryCommand {
+        get {
+            _selectDirectoryCommand = _selectDirectoryCommand ?? ReactiveCommand.CreateFromTask(async () =>
+            {
+                if (Avalonia.Application.Current.ApplicationLifetime is IClassicDesktopStyleApplicationLifetime desktop)
+                {
+                    var result = await new OpenFolderDialog()
+                    {
+                        Title = "Select folder",
+                        Directory = this.DirPath,
+                    }.ShowAsync(desktop.MainWindow);
+                    if (!string.IsNullOrEmpty(result)) {
+                        this.DirPath = result;
+                    }
+                }
+            });
+            return _selectDirectoryCommand;
+        }
+    }
+
+    public ICommand ShowExifCommand {
+        get {
+            _showExifCommand = _showExifCommand ?? ReactiveCommand.CreateFromTask(async () =>
+            {
+                string filePath = Path.Combine(DirPath, "img.jpg");
+                var file = ImageFile.FromFile(filePath);
+                var description = (string)file.Properties.Get(ExifTag.ImageDescription).Value;
+                ExifDescription = description;
+            });
+            return _showExifCommand;
+        }
+    }
+
     public string DirPath
     {
-        get 
+        get
         {
-            return appSettings.DirPath;
+            return appSettings?.DirPath ?? "";
         }
         set {
             appSettings.DirPath = value;
             SettingsService.SaveSettings(appSettings);
             this.RaisePropertyChanged(nameof(DirPath));
         }
+    }
+
+    public string? ExifDescription
+    {
+        get 
+        {
+            return _exifDescription;
+        }
+        set 
+        {
+            this.RaiseAndSetIfChanged(ref _exifDescription, value);
+        }
+
     }    
 }
--- Views/MainWindow.axaml.cs
+++ Views/MainWindow.axaml.cs
@@ -9,35 +9,9 @@ using ExifLibrary;
 namespace ExifEditor.Views;
 
 public partial class MainWindow : Window
-{   
-    public static MainWindow? mainWindow;
-
+{
     public MainWindow()
     {
         InitializeComponent();
-        mainWindow = this;
-    }
-
-    private void OnButtonClick(object sender, RoutedEventArgs e)
-    {
-        //string filePath = Path.Combine(appSettings.DirPath, "img.jpg");
-
-        //var file = ImageFile.FromFile(filePath);
-        //var description = (string)file.Properties.Get(ExifTag.ImageDescription).Value;
-        //outputText.Text = description;
-    }
-
-    private async void OnSelectDirectoryClick(object sender, RoutedEventArgs e)
-    {
-        var viewModel = DataContext as MainWindowViewModel;
-        var result = await new OpenFolderDialog()
-        {
-            Title = "Select folder",
-            Directory = viewModel.DirPath,
-        }.ShowAsync(mainWindow);
-
-        if (!string.IsNullOrEmpty(result)) {
-            viewModel.DirPath = result;
-        }
     }
 }
