b970e9da6a16107f14a437bd17532299f6399900
Michał Szyma <devszyma@gmail.com>
2023-09-19 00:04:11 +0200

    Add asterisk character to inform that property of image was changed.

--- ViewModels/MainWindowViewModel.cs
+++ ViewModels/MainWindowViewModel.cs
@@ -10,6 +10,7 @@ using System.IO;
 using System.Collections.Generic;
 using System.Collections.ObjectModel;
 using Avalonia.Media.Imaging;
+using Avalonia.Platform;
 
 namespace ExifEditor.ViewModels;
 
